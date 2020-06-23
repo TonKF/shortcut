be8f1e16fb567b9dd94ab3da1c9ec9184ed01d6f
TonKF <67007620+TonKF@users.noreply.github.com>
2020-06-23 17:15:21 +0900

    Update election.sh

--- election.sh
+++ election.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 #VARIABLES
-PUBKEY=$(cat ~/ton-keys/elections/localhost-request-dump2 | grep 'public key' | awk '{print $11}' | tr '[:upper:]' '[:lower:]')
+HOSTNAME="localhost"
+PUBKEY=$(cat ~/ton-keys/elections/$HOSTNAME-request-dump2 | grep 'public key' | awk '{print $11}' | tr '[:upper:]' '[:lower:]')
 PUBLICKEY=$(printf "0x%s" "$PUBKEY")
 checkPreparedElection=$(cd ~/net.ton.dev/tonos-cli/target/release && ./tonos-cli runget -1:3333333333333333333333333333333333333333333333333333333333333333 participates_in $PUBLICKEY | grep Result | awk '{print $2}' | tr -d '[' | tr -d '"' | tr -d ']')
 ACTIVE_ELECTION_ID=$(cat ~/ton-keys/elections/active-election-id)
+unconvertedTokens=$(printf "%d\n" "$checkPreparedElection")
+convertedTokens=$(expr "$unconvertedTokens" / 1000000000)
 
-function stakedTokens () {
-        printf "%d\n" "$checkPreparedElection"
-}
+if [ "$unconvertedTokens" != "0" ]; then
+        echo "Election participation confirmed!"
+        printf "Active Election ID: "
+        echo "$ACTIVE_ELECTION_ID"
 
-echo "Election participation confirmed!"
-printf "Active Election ID: "
-echo "$ACTIVE_ELECTION_ID"
-printf "Staked Tokens: "
-stakedTokens
+        printf "Staked Tokens: "
+        echo $convertedTokens
+elif [ "$unconvertedTokens" == "0" ]; then
+        echo "Election failed!"
+        printf "Active Election ID: "
+        echo "$ACTIVE_ELECTION_ID"
+else
+        echo "Unknown Error"
+fi
